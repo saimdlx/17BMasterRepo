c1a40a464c244db41a51b5cb2c55f855015f549b
saim <1629saim@gmail.com>
2026-03-05 21:03:57 -0800

    (add) homework progress

--- 17BMasterFile/quickwrite.cpp
+++ 17BMasterFile/quickwrite.cpp
@@ -4,9 +4,10 @@
  * Created on: February 25, 2026
  * Purpose:  Purpose
  */
-
 //System Libraries Here
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 //User Libraries Here
@@ -17,8 +18,20 @@ using namespace std;
 
 //Program Execution Begins Here
 int main() {
-
-    cout << "test flag";
     
+    float angle = 0;
+    float angcos = 0;
+    float angsin = 0;
+    float angtan = 0;
+
+    cout << "Input angle in degrees" << endl;
+    cin >> angle;
+    float rad;
+
+    rad = angle * (M_PI/180);
+    cout << fixed;
+    cout << "Cos: " << showpoint << setprecision(4) << cos(rad) << noshowpoint << endl;
+    cout << "Sin: " << showpoint << setprecision(4) << sin(rad) << noshowpoint << endl;
+    cout << "Tan: " << showpoint << setprecision(4) << tan(rad) << noshowpoint << endl;
     return 0;
 }
